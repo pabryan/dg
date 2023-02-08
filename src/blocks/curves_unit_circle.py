2c0887a54cd3ad72619097d1ebce18953490e905
Paul Bryan <pabryan@gmail.com>
2023-02-08 12:46:36 +1100

    Create separate svg figs for unit circle example.

--- src/blocks/curves_unit_circle.py
+++ src/blocks/curves_unit_circle.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import pandas as pd
 import numpy as np
 import scipy as sp
@@ -14,6 +11,7 @@ def arrowhead(x, y, th, **kwargs):
     ax = x - eps*np.cos(th)
     ay = y - eps*np.sin(th)
     return dict(x=x, y=y, ax=ax, ay=ay, text="", showarrow=True, arrowhead=1, arrowsize=2, **kwargs)
+
 th = np.linspace(-np.pi, np.pi, 100)
 ph = np.linspace(0.0, np.sqrt(2*np.pi), 100)
 
@@ -21,22 +19,25 @@ unit_circle = pd.DataFrame({'th' : th, 'x': np.cos(th), 'y': np.sin(th)})
 unit_circle_alt = pd.DataFrame({'ph' : ph, 'x': np.cos(ph**2), 'y': -np.sin(ph**2)})
 
 colors = px.colors.qualitative.Plotly
-fig = make_subplots(rows=2, cols=2, row_heights=[0.8, 0.2], subplot_titles = (r"$\gamma(t) = (\cos(t), \sin(t)) \quad -\pi < t < \pi$", r"$\gamma(t) = (\cos(t^2), -\sin(t^2)) \quad 0 < t < \sqrt{2\pi}$"))
+fig = make_subplots(rows=2, cols=1, row_heights=[0.8, 0.2])
 fig.update_annotations(yshift=20)
+fig_alt = make_subplots(rows=2, cols=1, row_heights=[0.8, 0.2])
+fig_alt.update_annotations(yshift=20)
 
+#fig.update_xaxes(range=[-1.5, 1.5], row=1, col=1)
 fig.update_yaxes(scaleanchor="x1", scaleratio=1, row=1, col=1)
 fig.update_xaxes(zeroline=False, showline=False, range=[-(np.pi + 0.5), np.pi + 0.5], tickmode = "array", tickvals=np.linspace(-np.pi, np.pi, 5), ticktext=[r"$-\pi$", ".", ".", ".", r"$\pi$"], row=2, col=1)
 fig.update_yaxes(showgrid=False, zeroline=False, showline=False, showticklabels=False, row=2, col=1)
 
-fig.update_yaxes(scaleanchor="x2", scaleratio=1, row=1, col=2)
-fig.update_xaxes(zeroline=False, showline=False, range=[-0.5, np.sqrt(2*np.pi) + 0.5], tickmode = "array", tickvals=np.linspace(0.0, np.sqrt(2*np.pi), 5), ticktext=[r"$0$", ".", ".", ".", r"$\sqrt{2\pi}$"], row=2, col=2)
-fig.update_yaxes(showgrid=False, zeroline=False, showline=False, showticklabels=False, row=2, col=2)
+fig_alt.update_yaxes(scaleanchor="x1", scaleratio=1, row=1, col=1)
+fig_alt.update_xaxes(zeroline=False, showline=False, range=[-0.5, np.sqrt(2*np.pi) + 0.5], tickmode = "array", tickvals=np.linspace(0.0, np.sqrt(2*np.pi), 5), ticktext=[r"$0$", ".", ".", ".", r"$\sqrt{2\pi}$"], row=2, col=1)
+fig_alt.update_yaxes(showgrid=False, zeroline=False, showline=False, showticklabels=False, row=2, col=1)
 
 fig.add_trace(go.Scatter(x=unit_circle["x"], y=unit_circle["y"], name="standard unit circle", showlegend=False, line=dict(color=colors[0])), row=1, col=1)
 fig.add_trace(go.Scatter(x=unit_circle["th"], y=np.zeros_like(unit_circle["th"]), showlegend=False, line=dict(color=colors[0])), row=2, col=1)
 
-fig.add_trace(go.Scatter(x=unit_circle_alt["x"], y=unit_circle_alt["y"], name="alternative unit circle", showlegend=False, line=dict(color=colors[1])), row=1, col=2)
-fig.add_trace(go.Scatter(x=unit_circle_alt["ph"], y = np.zeros_like(unit_circle_alt["ph"]), showlegend=False, line=dict(color=colors[1])), row=2, col=2)
+fig_alt.add_trace(go.Scatter(x=unit_circle_alt["x"], y=unit_circle_alt["y"], name="alternative unit circle", showlegend=False, line=dict(color=colors[1])), row=1, col=1)
+fig_alt.add_trace(go.Scatter(x=unit_circle_alt["ph"], y = np.zeros_like(unit_circle_alt["ph"]), showlegend=False, line=dict(color=colors[1])), row=2, col=1)
 
 
 t0 = 0.0
@@ -61,17 +62,20 @@ fig.add_annotation(arrowhead(x1, y1, th1, xref="x1", yref="y1", axref="x1", ayre
 
 fig.add_trace(go.Scatter(x=[0.0], y=[0.0], mode="markers", showlegend=False, marker=dict(color="black", size=10)), row=2, col=1)
 fig.add_trace(go.Scatter(x=arcth, y=np.zeros_like(arcth), showlegend=False, line=dict(color="black")), row=2, col=1)
-fig.add_annotation(arrowhead(t1, 0, 0, xref="x3", yref="y3", axref="x3", ayref="y3"), row=2, col=1)
+fig.add_annotation(arrowhead(t1, 0, 0, xref="x2", yref="y2", axref="x2", ayref="y2"), row=2, col=1)
 
-fig.add_trace(go.Scatter(x=[x0], y=[y0], mode="markers", showlegend=False, marker=dict(color="black", size=10)), row=1, col=2)
-fig.add_trace(go.Scatter(x=np.cos(arcph**2), y=-np.sin(arcph**2), showlegend=False, line=dict(color="black")), row=1, col=2)
-fig.add_annotation(arrowhead(x1, y1, th1, xref="x2", yref="y2", axref="x2", ayref="y2"), row=1, col=2)
+fig_alt.add_trace(go.Scatter(x=[x0], y=[y0], mode="markers", showlegend=False, marker=dict(color="black", size=10)), row=1, col=1)
+fig_alt.add_trace(go.Scatter(x=np.cos(arcph**2), y=-np.sin(arcph**2), showlegend=False, line=dict(color="black")), row=1, col=1)
+fig_alt.add_annotation(arrowhead(x1, y1, th1, xref="x1", yref="y1", axref="x1", ayref="y1"), row=1, col=1)
 
-fig.add_trace(go.Scatter(x=[np.sqrt(2*np.pi)], y=[0.0], mode="markers", showlegend=False, marker=dict(color="black", size=10)), row=2, col=2)
-fig.add_trace(go.Scatter(x=arcph, y=np.zeros_like(arcph), showlegend=False, line=dict(color="black")), row=2, col=2)
-fig.add_annotation(arrowhead(p1, 0, -np.pi, xref="x4", yref="y4", axref="x4", ayref="y4"), row=2, col=2)
-fig.update_yaxes(range=[-1, 1], row=2, col=2)
+fig_alt.add_trace(go.Scatter(x=[np.sqrt(2*np.pi)], y=[0.0], mode="markers", showlegend=False, marker=dict(color="black", size=10)), row=2, col=1)
+fig_alt.add_trace(go.Scatter(x=arcph, y=np.zeros_like(arcph), showlegend=False, line=dict(color="black")), row=2, col=1)
+fig_alt.add_annotation(arrowhead(p1, 0, -np.pi, xref="x2", yref="y2", axref="x2", ayref="y2"), row=2, col=1)
+fig_alt.update_yaxes(range=[-1, 1], row=2, col=1)
 
-fig.update_layout(title_text="Parametrisations of the unit circle")
+fig.update_layout(title = {'text': r"$\gamma(t) = (\cos(t), \sin(t)), \quad -\pi < t < \pi$", 'x': 0.5, 'xanchor': 'center'})
+fig_alt.update_layout(title = {'text': r"$\gamma(t) = (\cos(t^2), -\sin(t^2)), \quad 0 < t < \sqrt{2\pi}$", 'x': 0.5, 'xanchor': 'center'})
 
-fig.write_html("../../docs/_includes/curves_unit_circle_plot.html", include_plotlyjs="cdn", include_mathjax=False, full_html=False)
+fig.write_image("../../docs/img/unit_circle_example.svg")
+fig_alt.write_image("../../docs/img/unit_circle_alt_example.svg")
+
