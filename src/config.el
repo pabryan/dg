fc93d78d78e10606c692515e5d03fea3a8037d14
Paul Bryan <pabryan@gmail.com>
2023-02-26 21:08:04 +1100

    Small updates to div classes in environments and removed .el from use-package.

--- src/config.el
+++ src/config.el
@@ -1,8 +1,8 @@
-(use-package org-special-block-extras.el)
+(use-package org-special-block-extras)
 
 (org-defblock env (type) (title "")
   "Create an environment with a title."
   (format (if (equal backend 'html)
-              "<div class=\"%s card my-3 mx-0\"><div class=\"card-header text-white\" style=\"background-color: #0059b3;\"><h5 class=\"card-title\">Definition: %s</h5></div><div class=\"card-body\">%s</div></div>"
+              "<div class=\"%s card\"><div class=\"card-header\"><h5 class=\"card-title\">%s</h5></div><div class=\"card-body\">%s</div></div>"
             "\\begin{%s}[%s] %s \\end{%s}")
           type title contents type))
