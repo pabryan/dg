e0738e4128725f386046af0d8530fe0e8918103a
Paul Bryan <pabryan@gmail.com>
2023-03-01 19:38:40 +1100

    Add colon to title if not empty.

--- src/config.el
+++ src/config.el
@@ -3,6 +3,6 @@
 (org-defblock env (type) (title "")
   "Create an environment with a title."
   (format (if (equal backend 'html)
-              "<div class=\"%s card\"><div class=\"card-header\"><h5 class=\"card-title\">%s</h5></div><div class=\"card-body\">%s</div></div>"
+              (concat "<div class=\"%s card\"><div class=\"card-header\"><h5 class=\"card-title\">" (if (equal title "") "%s" " : %s") "</h5></div><div class=\"card-body\">%s</div></div>")
             "\\begin{%s}[%s] %s \\end{%s}")
-          type title contents type))
+	  type title contents type))
