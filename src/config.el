e5fa3953f8ff5953695022bd8cc4db868a4b5939
Paul Bryan <pabryan@gmail.com>
2023-03-06 12:14:12 +1100

    Added org-special-block-extra for slide speaker notes.

--- src/config.el
+++ src/config.el
@@ -6,3 +6,7 @@
               (concat "<div class=\"%s card\"><div class=\"card-header\"><h5 class=\"card-title\">" (if (equal title "") "%s" " : %s") "</h5></div><div class=\"card-body\">%s</div></div>")
             "\\begin{%s}[%s] %s \\end{%s}")
 	  type title contents type))
+
+(org-defblock notes () ()
+  "Environment for speaker notes"
+  (format "<aside class=\"notes\">%s</aside>" contents))
