ec322813fc2b047b866f6245add34104bbc68fbb
Paul Bryan <pabryan@gmail.com>
2023-05-21 22:47:57 +1000

    Rolled back 831f0f8 by reinstating delete html generated file when publishing
    Otherwise my source gets polluted with auto gen html files which would
    need to be ignored on a case by case basis since I don't want to
    ignore all html files. Too much maintenance!

--- build_tools/publish_slide.sh
+++ build_tools/publish_slide.sh
@@ -10,7 +10,7 @@ build_dir="${HOME}/dg-src/build/html/slides/${slides_dir}"
 pub_dir="${HOME}/dg/docs/slides"
 build_script="${HOME}/dg-src/build_tools/org_html_to_revealjs.py"
 
-cp "${slides_file}" "${build_dir}/${build_file}"
+mv "${slides_file}" "${build_dir}/${build_file}"
 cd "${build_dir}/"
 "${build_script}" "${build_file}" "${export_file}"
 mv "${export_file}" "${pub_dir}/${slides_dir}"
