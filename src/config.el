0ab92102f80e828d6c67032c53aa5ab98df1d45f
Paul Bryan <pabryan@gmail.com>
2024-02-07 20:26:15 +1100

    Starting from scratch. Removed old (and superfluous anyway) src.

--- src/config.el
+++
@@ -1,8 +0,0 @@
-(use-package org-special-block-extras)
-
-(org-defblock env (type) (title "")
-  "Create an environment with a title."
-  (format (if (equal backend 'html)
-              "<div class=\"%s card\"><div class=\"card-header\"><h5 class=\"card-title\">%s</h5></div><div class=\"card-body\">%s</div></div>"
-            "\\begin{%s}[%s] %s \\end{%s}")
-          type title contents type))
