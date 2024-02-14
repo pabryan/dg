144b6464baa798629464071b28e0d294793835e1
Paul Bryan <pabryan@gmail.com>
2024-02-14 17:28:07 +1100

    Added build conversion tools.

---
+++ src/build_tools/org_html_to_notes.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+from bs4 import BeautifulSoup
+import frontmatter
+
+def convert_section(s, soup):
+    sid = s["id"]
+    s["id"] = sid + "-item"
+    s["class"] = "accordion-item"
+
+    header = soup.new_tag("h1")
+    header["class"] = "accordion-header"
+    header["id"] = sid + "-header"
+    s.insert(0, header)
+
+    body = soup.new_tag("div")
+    body["id"] = sid + "-collapse"
+    body["class"] = "accordion-collapse collapse"
+    body["aria-labelledby"] = sid + "-header"
+    s.insert(1, body)
+
+    H = s.find("h2")
+    H.name = "button"
+    H["class"] = "accordion-button collapsed"
+    H["type"] ="button"
+    H["data-bs-toggle"] = "collapse"
+    H["data-bs-target"] = "#" + sid + "-collapse"
+    H["aria-expanded"] = "false"
+    H["aria-controls"] = sid + "-collapse"
+
+    header.insert(0, H)
+
+    B = s.find("div", class_="outline-text-2")
+    B["class"] = "accordion-body p-3 m-0"
+    B["id"] += "-body"
+
+    body.insert(0, B)
+
+def convert(html):
+    section_class = "outline-2"
+
+    soup = BeautifulSoup(html, "lxml")
+
+    sections = soup.find_all("div", class_=section_class)
+    if (len(sections) > 0):
+        s1 = sections[0]
+        accordion = soup.new_tag("accordion")
+        s1.insert_before(accordion)
+
+        for s in sections:
+            accordion.append(s)
+            convert_section(s, soup)
+
+    return (" ".join(str(c) for c in soup.body.children))
+
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    with open(infile, 'r') as f:
+        contents = frontmatter.load(f)
+
+    contents["layout"] = "notes"
+    contents.content = convert(contents.content)
+
+    with open(outfile, "w") as f:
+        f.write(frontmatter.dumps(contents))
---
+++ src/build_tools/org_html_to_revealjs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+from bs4 import BeautifulSoup
+
+def convert(infile, outfile):
+    classes = ["outline-2", "outline-3"]
+
+    with open(infile, 'r') as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+
+        for cls in classes:
+            divs = soup.find_all("div", class_=cls)
+            for div in divs:
+                div.name = "section"
+
+    body = "---" + os.linesep + "layout: slides" + os.linesep + "---" + os.linesep + os.linesep
+
+    for child in soup.body.children:
+        body += str(child)
+
+        with open(outfile, "w") as file:
+            file.write(body)
+
+if __name__ == "__main__":
+    convert(sys.argv[1], sys.argv[2])
+
